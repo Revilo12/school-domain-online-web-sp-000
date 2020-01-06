038e3b9f48f6d880029d107d6786a7b83d52aff0
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-06 18:48:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    roster[grade] = [] if roster[grade] = nil 
+    roster[grade] << name
+  end
+  
+end
