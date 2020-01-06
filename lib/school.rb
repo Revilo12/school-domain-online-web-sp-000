ed64447bf74d93069868ca2f8df5af0babf96612
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-06 18:52:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
   end
 
   def add_student(name, grade)
-    roster[grade] = [] if roster[grade] = nil
-    binding.pry
+    if roster[grade] = nil
+      roster[grade] = [] 
     roster[grade] << name
   end
 
